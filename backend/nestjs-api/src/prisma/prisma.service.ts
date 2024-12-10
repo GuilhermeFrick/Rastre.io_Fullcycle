adb8849281025aff13b6e4ccd925ce42fd21ddf8
GuilhermeFrick <frickoliveira.ee@gmail.com>
2024-12-10 05:13:30 +0000

    change: aorrigindo imports do prisma

--- backend/nestjs-api/src/prisma/prisma.service.ts
+++ backend/nestjs-api/src/prisma/prisma.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
-    async onModuleInit() {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  // Conexão com o banco de dados ao iniciar o módulo
+  async onModuleInit() {
     await this.$connect();
-    }
+    console.log('Prisma Client conectado ao banco de dados.');
+  }
+
+  // Fecha a conexão com o banco ao destruir o módulo
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Prisma Client desconectado do banco de dados.');
+  }
 }
